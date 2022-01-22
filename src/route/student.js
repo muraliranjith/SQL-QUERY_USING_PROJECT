3fb76dfa09cf05ef665c73eaa126bc298a206d67
muraliranjith <muralidharan@twilightsoftwares.com>
2022-01-22 16:54:18 +0530

    fEAT:Chanages

--- src/route/student.js
+++ src/route/student.js
@@ -3,16 +3,13 @@ const studentController = require('../controller/student.controller');
 const router = require('express').Router()
 
 router.post('/createtable',studentController.createTable)
+router.post('/mysql',studentController.createStudent);
 router.get('/mysql',studentController.getStudents);
 router.get('/picklike',studentController.pickLike);
 router.get('/fulltext',studentController.fultext);
 router.get('/order',studentController.order);
 router.get('/agecompair',studentController.age);
-
 router.get('/searching',studentController.searching);
-
-router.post('/mysql',studentController.createStudent);
-
 router.get('/mysql/:id',studentController.getStudentById);
 router.delete('/mysql/',studentController.deleteStudentById);
 router.put('/mysql/:id',studentController.updateStudentById)
